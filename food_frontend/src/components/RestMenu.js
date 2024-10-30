68f577ce4c71447feb0137dcf83962b88a599fd3
“RitulV” <rituljv@gmail.com>
2024-10-30 19:58:37 +0530

    made minor changes to structure

--- food_frontend/src/components/RestMenu.js
+++ food_frontend/src/components/RestMenu.js
@@ -17,46 +17,48 @@ const RestMenu = () => {
     setData(json.data);
   };
 
+  const resData = resInfo?.cards[2]?.card?.card?.info;
+
   return (
     <div className="res-menu-container">
       <div className="res-menu-path">
         <span>Home</span>
         <span>/</span>
-        <span>{resInfo?.cards[2]?.card?.card?.info?.city}</span>
+        <span>{resData?.city}</span>
         <span>/</span>
-        <span>{resInfo?.cards[2]?.card?.card?.info?.name}</span>
+        <span>{resData?.name}</span>
       </div>
 
       <div className="res-menu-name">
-        <h2>{resInfo?.cards[2]?.card?.card?.info?.name}</h2>
+        <h2>{resData?.name}</h2>
       </div>
 
       <div className="quick-info-card">
         <div className="quick-info-first">
           <img src="https://ritulv.github.io/image-hosting/star-logo.png"></img>
           <span>
-            {resInfo?.cards[2]?.card?.card?.info?.avgRatingString}
+            {resData?.avgRatingString}
             {" ("}
-            {resInfo?.cards[2]?.card?.card?.info?.totalRatingsString}
+            {resData?.totalRatingsString}
             {") "}
           </span>
           <span className="quick-info-first-dot">·</span>
-          <span>{resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage}</span>
+          <span>{resData?.costForTwoMessage}</span>
         </div>
 
         <div className="quick-info-second">
-          {resInfo?.cards[2]?.card?.card?.info?.cuisines.join(", ")}
+          {resData?.cuisines.join(", ")}
         </div>
 
         <div className="quick-info-third">
           <img src="https://ritulv.github.io/image-hosting/loc-pin.png"></img>
           <span>Outlet</span>
-          <p>{resInfo?.cards[2]?.card?.card?.info?.areaName}</p>
+          <p>{resData?.areaName}</p>
         </div>
 
         <div className="quick-info-fourth">
           <img src="https://ritulv.github.io/image-hosting/timer.png"></img>
-          <span>{resInfo?.cards[2]?.card?.card?.info?.sla?.slaString}</span>
+          <span>{resData?.sla?.slaString}</span>
         </div>
       </div>
 
